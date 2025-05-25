6634eb7cde5aacaf9d2e859753877a526cd44d62
Luis Daniel de la Rosa <luisdaniel.dlr.380@gmail.com>
2025-05-25 15:03:52 -0600

    welcome useDocumentTitle removed

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,13 +1,10 @@
-import useDocumentTitle from "@/hooks/useDocumentTitle"
-
 const Welcome = () => {
 
-  useDocumentTitle('Getting Started')
-
   return (
-    <div>
-        Bienvenidos
-    </div>
+    <>
+      <title>Welcome</title>
+      Bienvenidos
+    </>
   )
 }
 
